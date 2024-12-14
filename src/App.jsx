8f77a26658ba72e866daff0b651ebb5565881e10
akcsantos <65388001+akcsantos@users.noreply.github.com>
2024-12-14 09:40:13 +0800

    setting up environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Etchh</>;
+  return (
+    <div>
+      <Header />
+      <MainContainer />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

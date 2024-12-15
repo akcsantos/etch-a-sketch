1a65eae9b8175f6bf488cd25536ecd5707938faa
akcsantos <65388001+akcsantos@users.noreply.github.com>
2024-12-15 10:16:42 +0800

    add change in size and color. fix readability

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -3,16 +3,31 @@ import styles from "./mainContainer.module.css";
 
 export default function MainContainer() {
   const [bg, setBg] = useState("");
-  function changeBG() {
-    setBg("black");
+  const [size, setSize] = useState(16);
+
+  function changeBG(e) {
+    e.target.style.backgroundColor = "black";
+    e.target.style.border = "none";
   }
+
   return (
     <div className={styles.mainContainer}>
       <div
         className={styles.tiles}
-        style={{ backgroundColor: bg }}
+        style={{
+          gridTemplateColumns: `repeat(${size}, 1fr)`,
+          gridTemplateRows: `repeat(${size}, 1fr)`,
+        }}
         onMouseEnter={changeBG}
-      ></div>
+      >
+        {[...Array(size ** 2)].map((_, i) => (
+          <div
+            className={styles.cell}
+            key={crypto.randomUUID()}
+            onMouseEnter={changeBG}
+          />
+        ))}
+      </div>
     </div>
   );
 }
